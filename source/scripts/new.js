a7135d42a461870f112c26534b6728831a4b256b
beomi <latheledusjp@gmail.com>
2018-02-23 18:17:27 +0900

    update code for checking

--- source/scripts/new.js
+++ source/scripts/new.js
@@ -1,11 +1,7 @@
-var authURL = 'http://first.wifi.olleh.com/starbucks/index_kr.html';
-switch (window.location.href) {
-    case authURL:
-        document.querySelector('#agreement_agree').checked = true;
-        document.querySelector('#purpose_agree').checked = true;
+var url = 'http://first.wifi.olleh.com/starbucks/index_kr.html';
 
-        document.querySelector('#contents > div.con_agree > div.con_box > fieldset > p > a > span').click()
-        break;
-    default:
-        window.location.href = authURL;
+if (window.location.href === url) {
+    document.querySelector('#agreement_agree').checked = true;
+    document.querySelector('#purpose_agree').checked = true;
+    document.querySelector('#contents > div.con_agree > div.con_box > fieldset > p > a > span').click()
 }
