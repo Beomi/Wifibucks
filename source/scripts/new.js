5ec5bcfe7ed47071084153b507c516c70518b6ba
beomi <latheledusjp@gmail.com>
2018-02-23 18:34:33 +0900

    Fix for http/https both support

--- source/scripts/new.js
+++ source/scripts/new.js
@@ -1,6 +1,6 @@
-var url = 'http://first.wifi.olleh.com/starbucks/index_kr.html';
+var url = 'first.wifi.olleh.com/starbucks/index_kr.html';
 
-if (window.location.href === url) {
+if (window.location.href.includes(url)) {
     document.querySelector('#agreement_agree').checked = true;
     document.querySelector('#purpose_agree').checked = true;
     document.querySelector('#contents > div.con_agree > div.con_box > fieldset > p > a > span').click()
