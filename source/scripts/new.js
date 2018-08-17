78d9dce2749fe0a6af458d3ee2e49e89e788c4a4
Junbum Lee <jun@beomi.net>
2018-08-17 11:14:29 +0900

    Update new.js

--- source/scripts/new.js
+++ source/scripts/new.js
@@ -4,5 +4,5 @@ var url = 'first.wifi.olleh.com/starbucks/index_kr.html';
 if (window.location.href.includes(url)) {
     document.querySelector('#agreement_agree').checked = true;
     document.querySelector('#purpose_agree').checked = true;
-    document.querySelector('#contents > div.con_agree > div.con_box > fieldset > p > a > span').click()
+    goAct(); // Use global function
 }
